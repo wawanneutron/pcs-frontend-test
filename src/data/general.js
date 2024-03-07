6b52596d1f4c472b22560861fcf89eb7c59ba48b
wawanneutron <wawanneutron1331@gmail.com>
2024-03-07 11:38:09 +0700

    split data general

---
+++ src/data/general.js
@@ -0,0 +1,136 @@
+export const NOTIFICATIONS = [
+  {
+    title: "Reimbursement",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisici elit. Ex, illum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, illum.",
+    date: "Today",
+    read: false,
+  },
+  {
+    title: "Reimbursement",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisici elit. Ex, illum Lorem i.",
+    date: "Yesterday",
+    read: true,
+  },
+  {
+    title: "Reimbursement",
+    desc: "Lorem ipsum dolor sit am tetur, adipisici elit. Ex, illum Lorem ipsum dolor sit amet ",
+    date: "2022-10-06",
+    read: false,
+  },
+  {
+    title: "Sickness",
+    desc: " dolor sit amet consectetur, adipisici elit. Ex, illum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, ",
+    date: "2022-11-05",
+    read: false,
+  },
+  {
+    title: "Sickness",
+    desc: " dolor sit amet consectetur, adipisici elit. Ex, illum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, ",
+    date: "2022-11-05",
+    read: true,
+  },
+  {
+    title: "Sickness",
+    desc: " dolor sr sit amet consectetur, adipisicing elit. Ex, ",
+    date: "2022-11-05",
+    read: true,
+  },
+  {
+    title: "Overtime",
+    desc: " dolor sit amet consectetur, adipisici elit. Ex, illum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, ",
+    date: "2022-11-05",
+    read: false,
+  },
+  {
+    title: "Overtime",
+    desc: " dolor sit amet consectetur, adipisici elit. Ex, illum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, ",
+    date: "2022-11-05",
+    read: true,
+  },
+  {
+    title: "Overtime",
+    desc: " dolor sit amet consectetur, adipisici elit. Ex, illum Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, ",
+    date: "2022-11-05",
+    read: true,
+  },
+];
+
+export const NEWS = [
+  {
+    name: "Ade",
+    desc: "Lorem  dolor sit, amet consectetur adipisicing elit. Officia etquibusdam culpa. Repellendus, natus, ad in fugit ullam",
+  },
+  {
+    name: "Bayu",
+    desc: "architecto fugiat perspiciatis sunt accusamus suscipit! Iste est tenetur earum corrupti quidem! Cupiditate delectus labor",
+  },
+  {
+    name: "Ical",
+    desc: "Officia  culpa. Repellendus, natus, ad in fugit ullam",
+  },
+];
+
+export const USERS = [
+  {
+    name: "Ade",
+    location: "BSD",
+  },
+  {
+    name: "Bayu",
+    location: "BSD",
+  },
+  {
+    name: "Ical",
+    location: "BSD",
+  },
+  {
+    name: "Dewa",
+    location: "BSD",
+  },
+  {
+    name: "Agung",
+    location: "BSD",
+  },
+
+  {
+    name: "Luqman",
+    location: "BSD",
+  },
+  {
+    name: "Yogi",
+    location: "BSD",
+  },
+];
+
+export const MENUS = [
+  {
+    name: "Home",
+    icon: "mdi-home",
+    route: "/",
+    float: false,
+  },
+  {
+    name: "Attendence",
+    icon: "mdi-calendar-check",
+    route: "/attendence",
+    float: false,
+  },
+  {
+    name: "Check Out",
+    icon: "mdi-logout",
+    route: "/checkout",
+    float: true,
+  },
+  {
+    name: "Form",
+    icon: "mdi-list-box",
+    route: "/form",
+    float: false,
+  },
+  {
+    name: "Setting",
+    icon: "mdi-cog",
+    route: "/settings",
+    float: false,
+  },
+];
