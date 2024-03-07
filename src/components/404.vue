05cf55ae1c84d37d4dc46cf89ebc23a9df4b25ad
wawanneutron <wawanneutron1331@gmail.com>
2024-03-07 11:58:55 +0700

    page notfound

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,8 +1,25 @@
 <template>
-  <section class="bg-primary py-12">
-    <div class="d-flex justify-center align-center flex-column">
-      <h1 class="text-black text-xl-h1">404</h1>
-      <p class="text-blue-accent-1">page not found</p>
-    </div>
+  <section
+    class="notfound d-flex justify-center align-center flex-column py-12"
+  >
+    <h1 class="text-black text-h1 font-weight-bold">404</h1>
+    <p class="text-red text-h4 font-weight-bold">Page not found</p>
+
+    <v-btn
+      to="/"
+      variant="outlined"
+      float
+      rounded="50"
+      size="large"
+      class="text-capitalize mt-8"
+    >
+      <v-icon class="mr-2">mdi-arrow-left</v-icon> Back to home
+    </v-btn>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.notfound {
+  height: 100%;
+}
+</style>
